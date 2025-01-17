3a01dfddb514f92b92e6318ddc5db5cae8dedd50
Maze Winther <mazewinther@gmail.com>
2025-01-17 01:39:53 +0100

    fix: update reset password redirect link

--- src/app/auth/login-help/page.tsx
+++ src/app/auth/login-help/page.tsx
@@ -15,7 +15,7 @@ const helpOptions: HelpOption[] = [
     icon: <FaKey className="flex-shrink-0" />,
     title: "Reset your password",
     description: "We'll send you a link to create a new password",
-    href: "/reset-password",
+    href: "/auth/reset-password",
   },
 ];
 

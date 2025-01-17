2c911d12f203c912c8d1bf902121e8e74376913f
Maze Winther <mazewinther@gmail.com>
2025-01-17 01:32:25 +0100

    docs: add comments to auth configuration for clarity

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,3 +1,8 @@
+/**
+ * This is only the configuration for custom auth we've implemented beyond what Supabase offers.
+ * You can change more things in your Supabase dashboard under authentication.
+ */
+
 export const AUTH_CONFIG = {
   deviceVerification: {
     codeExpirationTime: 10,

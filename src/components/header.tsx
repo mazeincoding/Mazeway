bdd7bc7a97c554e03e60d9b73cf39a3b5709eb7d
Maze Winther <mazewinther@gmail.com>
2025-02-07 22:46:45 +0100

    feat(ui): reduce header title font size for refined layout

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -11,7 +11,7 @@ interface HeaderProps {
 export function Header({ isInitiallyLoggedIn }: HeaderProps) {
   return (
     <header className="flex items-center justify-between py-4 backdrop-blur-xl border-b sticky top-0 z-30 bg-background flex-1">
-      <h1 className="text-2xl font-bold">
+      <h1 className="text-xl mt-0.5 font-bold">
         <Link href="/">Auth</Link>
       </h1>
       {isInitiallyLoggedIn ? (

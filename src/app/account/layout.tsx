6d7454956441e8ce1e4f186de89b4071696848d3
Maze Winther <mazewinther@gmail.com>
2025-02-08 20:34:41 +0100

    fix(ui): adjust vertical padding in account layout

--- src/app/account/layout.tsx
+++ src/app/account/layout.tsx
@@ -32,7 +32,7 @@ export default function AccountLayout({
         />
         <div className="flex flex-1 w-full">
           <SettingsSidebar />
-          <div className="flex-1 px-4 md:px-8 py-12">
+          <div className="flex-1 px-4 md:px-8 py-10">
             <div className="max-w-4xl mx-auto">{children}</div>
           </div>
         </div>

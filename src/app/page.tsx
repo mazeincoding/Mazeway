d88aaf8d77eee5797ac8a0647b351d5d71cad985
Maze Winther <mazewinther@gmail.com>
2025-03-03 01:19:49 +0100

    feat(ui): polished hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Header } from "@/components/header";
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
@@ -7,20 +8,23 @@ export default function Home() {
     <div className="flex flex-col min-h-screen">
       <Header isInitiallyLoggedIn={false} />
       <main className="flex-grow flex items-center justify-center flex-col gap-6 px-8 py-16 text-center">
-        <div className="flex flex-col gap-3 max-w-xl mx-auto">
-          <h1 className="text-4xl font-bold text-center text-pretty">
-            Mazeway Demo
+        <div className="flex items-center flex-col gap-5 max-w-2xl mx-auto">
+          <Badge variant="outline" className="w-fit text-sm font-normal">
+            Demo
+          </Badge>
+          <h1 className="text-5xl font-semibold text-center text-pretty">
+            Complete auth foundation{" "}
+            <span className="text-muted-foreground">you can customize</span>
           </h1>
           <p className="block text-lg text-muted-foreground text-pretty">
-            See how complete auth feels. When you're ready, the auth is yours.
+            Drop in production-ready auth code with everything apps need
           </p>
         </div>
         <div className="flex gap-2">
-          <Link href="/auth/login">
-            <Button variant="secondary">Log in</Button>
-          </Link>
           <Link href="/auth/signup">
-            <Button>Sign up</Button>
+            <Button size="lg" className="text-base">
+              Try now
+            </Button>
           </Link>
         </div>
       </main>

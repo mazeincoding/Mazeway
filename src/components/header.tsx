77057fbd7bb39e41a2dd8766a330e06d5475a1e2
Maze Winther <mazewinther@gmail.com>
2025-03-03 23:43:30 +0100

    feat(ui): add GitHub and Twitter social links to header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { Button } from "./ui/button";
 import { UserDropdown } from "./user-dropdown";
+import { FaGithub, FaTwitter } from "react-icons/fa";
 
 interface HeaderProps {
   isInitiallyLoggedIn: boolean;
@@ -26,13 +27,27 @@ export function Header({ isInitiallyLoggedIn, sidebar }: HeaderProps) {
           />
         </Link>
       </div>
-      {isInitiallyLoggedIn ? (
-        <UserDropdown />
-      ) : (
-        <Link href="/auth/login">
-          <Button variant="secondary">Log in</Button>
-        </Link>
-      )}
+      <div className="flex items-center gap-4">
+        <div className="flex items-center gap-1">
+          <Link href="https://github.com/mazeincoding/mazeway" target="_blank">
+            <Button variant="ghost" size="icon">
+              <FaGithub className="h-5 w-5" />
+            </Button>
+          </Link>
+          <Link href="https://twitter.com/mazewinther1" target="_blank">
+            <Button variant="ghost" size="icon">
+              <FaTwitter className="h-5 w-5" />
+            </Button>
+          </Link>
+        </div>
+        {isInitiallyLoggedIn ? (
+          <UserDropdown />
+        ) : (
+          <Link href="/auth/login">
+            <Button>Try demo</Button>
+          </Link>
+        )}
+      </div>
     </header>
   );
 }
